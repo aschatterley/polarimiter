0995e37796daee65f325b3fffbca4e0ba1371239
Adam Simon Chatterley <aschatterley@gmail.com>
2017-04-23 01:09:28 +0200

    calculate rotating QWP scans

--- StokesFromWPScan.m
+++ StokesFromWPScan.m
@@ -1,8 +1,23 @@
-function [ S ] = StokesFromWPScan( intensity, angle, retardance )
-%StokesFromWPScan Retreive stokes from QWP scan
-%   Detailed explanation goes here
+function [ S, calculatedI ] = StokesFromWPScan( intensity, angle, retardance )
+%StokesFromWPScan Retreive stokes from QWP scan.
+%   intensity: intensity transmitted through polarizer
+%   angle: angles of waveplate
+%   retardance: retardance of WP in waves
+%   Returns the stokes vector in S, and the calcualted trace in calc.
 
 fftI = fft(intensity);
+
+%delete all other frequencies
+fftICalc = zeros(size(fftI));
+fftICalc(1) = real(fftI(1));
+fftICalc(3) = 1i*imag(fftI(3));
+fftICalc(5) = fftI(5);
+fftICalc(end-1) = 1i*imag(fftI(end-1));
+fftICalc(end-3) = fftI(end-3);
+
+
+calculatedI = real(ifft(fftICalc));
+
 
 a =    real(fftI(1)) / length(angle);
 b =  2*imag(fftI(3)) / length(angle);
